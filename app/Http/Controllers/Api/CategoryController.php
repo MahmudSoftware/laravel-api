39be24a1954cb8ebd611a69a339b9ac35f8fe604
mahmud <mahmudsoftware@gmail.com>
2025-01-21 17:52:25 +0600

    Create Category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(){
+       return response()->json([
+        'status' =>200,
+        'message'=>'Bd'
+       ]); 
+    }
+
+    public function store(Request $request){
+        return response()->json([
+            'form_request' =>$request->all()
+           ]); 
+    }
+}
